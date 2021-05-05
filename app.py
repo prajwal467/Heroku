e887a327fd363a1c784a19d6307fb69632b99981
prajwal467 <57497955+prajwal467@users.noreply.github.com>
2021-05-05 15:53:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,34 @@
-#This is Heroku Deployment Lectre
+# This is Heroku Deployment Lectre
 from flask import Flask, request, render_template
 import os
 import pickle
 
-print("Test")
-print("Test 2")
-print(os.getcwd())
+print("working dir:", os.getcwd())
 path = os.getcwd()
 
-with open('Models/Pickled_LR_Model', 'rb') as f:
+with open('Models/LR_Model.pkl', 'rb') as f:
     logistic = pickle.load(f)
 
+with open('Models/RF_Model.pkl', 'rb') as f:
+    randomforest = pickle.load(f)
+
+with open('Models/SVM_Model.pkl', 'rb') as f:
+    svm_model = pickle.load(f)
 
 
-
-def get_predictions(age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal, req_model):
-    mylist = [age,sex,cp,trestbps]
+def get_predictions(age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang,
+                    oldpeak, slope, ca, thal, req_model):
+    mylist = [age, sex, cp, trestbps, chol, fbs, restecg,
+              thalach, exang, oldpeak, slope, ca, thal]
     mylist = [float(i) for i in mylist]
     vals = [mylist]
 
     if req_model == 'Logistic':
-        #print(req_model)
         return logistic.predict(vals)[0]
-
-    
+    elif req_model == 'RandomForest':
+        return randomforest.predict(vals)[0]
+    elif req_model == 'SVM':
+        return svm_model.predict(vals)[0]
     else:
         return "Cannot Predict"
 
@@ -39,22 +44,35 @@ def homepage():
 @app.route('/', methods=['POST', 'GET'])
 def my_form_post():
     if request.method == 'POST':
-        price = request.form['age']
-        Tax = request.form['sex']
-        Driver_Age = request.form['cp']
-        Licence_Length_Years = request.form['trestbps']
+        age = request.form['age']
+        sex = request.form['sex']
+        cp = request.form['cp']
+        trestbps = request.form['trestbps']
+        chol = request.form['chol']
+        fbs = request.form['fbs']
+        restecg = request.form['restecg']
+        thalach = request.form['thalach']
+        exang = request.form['exang']
+        oldpeak = request.form['oldpeak']
+        slope = request.form['slope']
+        ca = request.form['ca']
+        thal = request.form['thal']
         req_model = request.form['req_model']
 
-        target = get_predictions(age, sex,cp,trestbps, req_model)
+        target = get_predictions(age, sex, cp, trestbps, chol, fbs,
+                                 restecg, thalach, exang, oldpeak, slope,
+                                 ca, thal, req_model)
 
-        if target==1:
-            heart_disease = 'Patient is likely to get heart disease'
+        if target == 1:
+            sale_making = 'Presence of Heart Disease'
         else:
-            heart_disease = 'Patient is unlikely to get heart disease'
+            sale_making = 'Absence of Heart Disease'
 
-        return render_template('home.html', target = target, sale_making = heart_disease)
+        return render_template('home.html', target=target,
+                               sale_making=sale_making)
     else:
         return render_template('home.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
